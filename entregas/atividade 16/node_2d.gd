6407df0feceaf5e4047de3fab82254b86eeb1a61
alexgithoioi <documentosalex1120@gmail.com>
2025-08-12 12:06:51 -0300

    atividade 16
    ela denovo

---
+++ entregas/atividade 16/node_2d.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	print("Hellow, Alex Ramos")
+
+
+func _on_button_button_up() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_toggled(toggled_on: bool) -> void:
+	pass # Replace with function body.
